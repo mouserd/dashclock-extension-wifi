5d02c23aff5abd5513a29d673dff0336272fe8a2
David Mouser <david.mouser@gmail.com>
2014-07-25 20:29:55 +1000

    Added catch attempts back in until root cause can be found.

--- src/main/java/com/pixelus/dashclock/ext/wifi/broadcast/WifiSignalStateBroadcastReceiver.java
+++ src/main/java/com/pixelus/dashclock/ext/wifi/broadcast/WifiSignalStateBroadcastReceiver.java
@@ -3,31 +3,33 @@ package com.pixelus.dashclock.ext.wifi.broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import com.crashlytics.android.Crashlytics;
 import com.pixelus.dashclock.ext.wifi.WifiExtension;
 
 public class WifiSignalStateBroadcastReceiver extends BroadcastReceiver {
 
-    private static final String TAG = WifiSignalStateBroadcastReceiver.class.getName();
+  private static final String TAG = WifiSignalStateBroadcastReceiver.class.getName();
 
-    private WifiExtension extension;
+  private WifiExtension extension;
 
-    public WifiSignalStateBroadcastReceiver(final WifiExtension extension) {
+  public WifiSignalStateBroadcastReceiver(final WifiExtension extension) {
 
-        this.extension = extension;
+    this.extension = extension;
+  }
+
+  @Override
+  public synchronized void onReceive(Context context, Intent intent) {
+
+    Log.d(TAG, "Received broadcast " + intent.getAction());
+    try {
+      extension.onUpdateData();
+    } catch (NullPointerException e) {
+      // Every so often an exception seems to be thrown by the DashClock api.
+      // It seems that this exception is timing related.  Catch and log it for now!
+      Crashlytics.log("NullPointerException caught when updating dashclock following receiving broadcast: "
+          + intent.toString());
+      Crashlytics.logException(e);
     }
-
-    @Override
-    public synchronized void onReceive(Context context, Intent intent) {
-
-//      Log.d(TAG, "Received broadcast " + intent.getAction());
-      //try {
-        extension.onUpdateData();
-//      } catch (NullPointerException e) {
-//        // Every so often an exception seems to be thrown by the DashClock api.
-//        // It seems that this exception is timing related.  Catch and log it for now!
-//        Crashlytics.log("NullPointerException caught when updating dashclock following receiving broadcast: "
-//          + intent.toString());
-//        Crashlytics.logException(e);
-//      }
-    }
+  }
 }
--- src/main/java/com/pixelus/dashclock/ext/wifi/broadcast/WifiStateBroadcastReceiver.java
+++ src/main/java/com/pixelus/dashclock/ext/wifi/broadcast/WifiStateBroadcastReceiver.java
@@ -3,6 +3,7 @@ package com.pixelus.dashclock.ext.wifi.broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import com.crashlytics.android.Crashlytics;
 import com.pixelus.dashclock.ext.wifi.WifiExtension;
 
 public class WifiStateBroadcastReceiver extends BroadcastReceiver {
@@ -19,14 +20,14 @@ public class WifiStateBroadcastReceiver extends BroadcastReceiver {
   @Override
   public synchronized void onReceive(Context context, Intent intent) {
 
-//      try {
-    extension.onUpdateData(WifiExtension.UPDATE_REASON_FORCED);
-//      } catch (NullPointerException e) {
-//        // Every so often an exception seems to be thrown by the DashClock api.
-//        // It seems that this exception is timing related.  Catch and log it for now!
-//        Crashlytics.log("NullPointerException caught when updating dashclock following receiving broadcast: "
-//          + intent.toString());
-//        Crashlytics.logException(e);
-//      }
+    try {
+      extension.onUpdateData(WifiExtension.UPDATE_REASON_FORCED);
+    } catch (NullPointerException e) {
+      // Every so often an exception seems to be thrown by the DashClock api.
+      // It seems that this exception is timing related.  Catch and log it for now!
+      Crashlytics.log("NullPointerException caught when updating dashclock following receiving broadcast: "
+          + intent.toString());
+      Crashlytics.logException(e);
+    }
   }
 }
